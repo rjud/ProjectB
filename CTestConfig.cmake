e5fb131288716042fb3177c8bbb6465ed8fbeb76
J. Bedouet <j.bedouet@free.fr>
2014-03-22 17:54:17 +0100

    Add CTest files

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,5 +1,5 @@
 set(CTEST_PROJECT_NAME "ProjectC")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "open.cdash.org")
-set(CTEST_DROP_LOCATION "submit.php?project=PublicDashboard")
+set(CTEST_DROP_LOCATION "/submit.php?project=PublicDashboard")
 set(CTEST_DROP_SITE_CDASH TRUE)
